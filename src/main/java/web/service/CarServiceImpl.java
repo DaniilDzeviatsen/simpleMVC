c7351863382cec049bec9640eb0aec00ce921fca
DaniilDzeviatsen <deviat.daan@gmail.com>
2024-06-17 16:51:10 +0300

    final after remarks

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,10 @@
-package web.config.service;
+package web.service;
 
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
